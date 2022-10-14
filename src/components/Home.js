ff9fe8551d5f393a638dfe2738d3a20411162b02
amschel99 <6290.2020@students.ku.ac.ke>
2022-10-14 14:13:49 +0300

    modified the card

--- src/components/Home.js
+++ src/components/Home.js
@@ -20,38 +20,36 @@ setVideos(data)
 getVideos()
     },[])
 
-    const Container=(props)=><Grid container {...props}/>
+    const Container=(props)=><Grid   container {...props}/>
         const Item=(props)=><Grid item {...props}/>
   return (
-    <Container spacing={4}>
+    <Container spacing={6}>
     {videos.map((video)=>{
-        return <Item xs={12}  sm={6} md={3} key={video.id}>
+        return <Item xs={12}  sm={3} key={video.id}>
         
   
 
-    <Card >
+    <Card  sx={{height:'440px', backgroundColor:'#FFD384'}}>
 <CardHeader
+sx={{color:'#FF884B', fontFamily:"'Playfair Display SC',sans-serif"}}
 title={video.name}
 />
-        <CardMedia 
+        <CardMedia
+        
         component='img'
       image={`http://localhost:8800${video.poster}`}
        alt={video.name}
       />
       <CardContent>
-<Typography variant="body2">
-    The theory of probability had its origin in gambling and games of chance. It owes 
-much to the curiosity of gamblers who pestered their friends in the mathematical 
-world with all sorts of questions. Unfortunately, this association with gambling contributed to very slow and sporadic growth of probability theory as a mathematical 
-discipline. The mathematicians of the day took little or no interest in the development of any theory but looked only at the combinatorial reasoning involved in each 
-problem.
+<Typography variant="body2" sx={{fontFamily: '"Lato", sans-serif'}}>
+ {video.description}
 </Typography>
       </CardContent>
 
    <CardActions>
     <IconButton>
        
-            <Button startIcon={<PlayCircle/>} href={`player/${video.id}`} variant="contained">Watch Course</Button>
+            <Button sx={{backgroundColor:'#FF884B'}} startIcon={<PlayCircle/>} href={`player/${video.id}`} variant="contained">Watch Course</Button>
 
        
     
